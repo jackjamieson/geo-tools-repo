41b17393de48bdb867a84bc8584c51f732e6b158
jackjamieson <jamieson.jack@gmail.com>
2015-03-25 12:51:35 -0400

    added multi file support to xml gen

--- js/xml.js
+++ js/xml.js
@@ -2,6 +2,87 @@
 var opencdata = "<![CDATA[ ";
 var closecdata = " ]]>";
 
+var fileList = [];
+var zipFile;
+
+var isUsingZip = false;
+
+// reading the zip data
+(function () {
+    if (!window.FileReader || !window.ArrayBuffer) {
+        $("#error_block").removeClass("hidden").addClass("show");
+        return;
+    }
+
+
+    var $result = $("#result");
+    $("#file").on("change", function(evt) {
+        isUsingZip = true;
+        fileList.length = 0;
+        // remove content
+        $result.html("");
+        // be sure to show the results
+        $("#result_block").removeClass("hidden").addClass("show");
+
+        // see http://www.html5rocks.com/en/tutorials/file/dndfiles/
+
+        var files = evt.target.files;
+        for (var i = 0, f; f = files[i]; i++) {
+
+            var reader = new FileReader();
+
+            // Closure to capture the file information.
+            reader.onload = (function(theFile) {
+                return function(e) {
+                    var $title = $("<h4>", {
+                        text : theFile.name
+                    });
+                    zipFile = theFile.name.substring(0, theFile.name.indexOf('.zip'));
+                    $result.append($title);
+                    var $fileContent = $("<ul>");
+                    try {
+
+                        var dateBefore = new Date();
+                        // read the content of the file with JSZip
+                        var zip = new JSZip(e.target.result);
+                        var dateAfter = new Date();
+
+                        $title.append($("<span>", {
+                            text:" (parsed in " + (dateAfter - dateBefore) + "ms)"
+                        }));
+
+                        // that, or a good ol' for(var entryName in zip.files)
+                        $.each(zip.files, function (index, zipEntry) {
+
+                            var fileName = zipEntry.name.substring(zipEntry.name.indexOf('/')+1, zipEntry.name.length);
+                            if(fileName != 'Thumbs.db'){
+                                $fileContent.append($("<li>", {
+                                    text : zipEntry.name
+                                }));
+                                fileList.push(fileName); // add the filename to the array so we can count them later and use name
+                            }
+                            // the content is here : zipEntry.asText()
+                        });
+                        // end of the magic !
+
+                    } catch(e) {
+                        $fileContent = $("<div>", {
+                            "class" : "alert alert-danger",
+                            text : "Error reading " + theFile.name + " : " + e.message
+                        });
+                    }
+                    $result.append($fileContent);
+                }
+            })(f);
+
+            // read the file !
+            // readAsArrayBuffer and readAsBinaryString both produce valid content for JSZip.
+            reader.readAsArrayBuffer(f);
+            // reader.readAsBinaryString(f);
+        }
+    });
+})();
+
 
 function element(name, content, inner){
 
@@ -23,6 +104,7 @@ function element(name, content, inner){
 
 $('#xml').click(function() {
 
+    var zip = new JSZip();
     //reset the link so it doesn't bypass the required check
     this.href = "#";
 
@@ -100,23 +182,36 @@ $('#xml').click(function() {
         header += element("datasetReferenceDate", $('#date').val());
         header += "</sample>";
 
-        var textFile = null,
-            makeTextFile = function (text) {
-                var data = new Blob([text], {type: 'text/xml'});
+        if(!isUsingZip){
+            //needed to generate a single file.  may still be necesary
+            var textFile = null,
+                makeTextFile = function (text) {
+                    var data = new Blob([text], {type: 'text/xml'});
 
-                // If we are replacing a previously generated file we need to
-                // manually revoke the object URL to avoid memory leaks.
-                if (textFile !== null) {
-                    window.URL.revokeObjectURL(textFile);
-                }
+                    // If we are replacing a previously generated file we need to
+                    // manually revoke the object URL to avoid memory leaks.
+                    if (textFile !== null) {
+                        window.URL.revokeObjectURL(textFile);
+                    }
 
-                textFile = window.URL.createObjectURL(data);
+                    textFile = window.URL.createObjectURL(data);
 
-                return textFile;
-            };
+                    return textFile;
+                };
+            this.href = makeTextFile(header);
+        }
+        else{
+            //var xmlFile = makeTextFile(header);
+            for(var i = 0; i < fileList.length; i++)
+            {
+                zip.file(fileList[i].substring(0, fileList[i].indexOf('.')) + "-xml.xml", header);
+
+            }
+            var content = zip.generate({type:"blob"});
+            saveAs(content, zipFile + "-xml.zip");
+        }
 
 
-        this.href = makeTextFile(header);
     }
 
 
