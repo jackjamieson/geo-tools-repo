4e191b716bc71a7f99d1523066ebd8808b2a573c
jackjamieson <jamieson.jack@gmail.com>
2015-07-23 11:08:37 -0400

    added trend and plunge from pitch calc

--- js/trendplunge.js
+++ js/trendplunge.js
@@ -1,7 +1,4 @@
 
-var output = "";
-var outputTrend = "";
-
 Math.degrees = function(rad)
 {
      return rad*(180/Math.PI);
@@ -10,6 +7,97 @@ Math.degrees = function(rad)
 Math.radians = function(deg)
 {
      return deg * (Math.PI/180);
+}
+
+
+var output = "";
+var outputTrend = "";
+
+
+// finds plunge from pitch and dip, params must be in radians
+function findPlunge(pitch, dip){
+    pitch = Math.radians(pitch);
+    dip = Math.radians(dip);
+
+    var re = Math.sin(pitch) * Math.sin(dip);
+    var re2 = Math.degrees(fnis(re));
+
+    return re2 + .5;
+}
+
+function findTrend(strike, dipdir, pitchdir, pitch, dip){
+
+    strike = Math.radians(strike);
+    pitch = Math.radians(pitch);
+    dip = Math.radians(dip);
+
+    var bb = Math.tan(pitch) * Math.cos(dip);
+    bb = Math.atan(bb);
+
+    //replace and spaces found in the dip direction or pitch direction
+    dipdir = dipdir.replace(/ +/g,"");
+    pitchdir = pitchdir.replace(/ +/g,"");
+
+    var isSouth = /^S$/i;
+    var isNorth = /^N$/i;
+    var isEast = /^E$/i;
+    var isWest = /^W$/i;
+
+    var isSouthEast = /^SE$/i;
+    var isSouthWest = /^SW$/i;
+    var isNorthWest = /^NW$/i;
+    var isNorthEast = /^NE$/i;
+
+
+    if(strike > 1.570796 && isSouth.test(dipdir) && isSouthEast.test(pitchdir)){
+        return postTrend(strike+bb);
+    }
+    if(strike < 1.570796 && isNorth.test(dipdir) && isSouthWest.test(pitchdir)){
+        return postTrend(strike+bb+3.141593);
+    }
+    if(strike < 1.570796 && isNorth.test(dipdir) && isNorthEast.test(pitchdir)){
+        return postTrend(strike-bb);
+    }
+    if(strike > 1.570796 && isNorth.test(dipdir) && isSouthEast.test(pitchdir)){
+        return postTrend(strike-bb);
+    }
+    if(strike < 1.570796 && isSouth.test(dipdir) && isNorthEast.test(pitchdir)){
+        return postTrend(strike+bb);
+    }
+    if(strike > 1.570796 && isSouth.test(dipdir) && isNorthWest.test(pitchdir)){
+        return postTrend(strike+bb+3.141593+3.141593);
+    }
+    if(strike > 1.570796 && isNorth.test(dipdir) && isNorthWest.test(pitchdir)){
+        return postTrend(strike+bb+3.141593);
+    }
+    if(strike < 1.570796 && isSouth.test(dipdir) && isSouthWest.test(pitchdir)){
+        return postTrend(strike-bb+3.141593);
+    }
+    else{
+        return "Error";
+    }
+
+}
+
+function postTrend(trend){
+    if(trend > 6.283186)
+    {
+        trend = trend - 6.283186;
+    }
+    if(trend < 0){
+        trend = trend + 6.283186;
+    }
+
+    trend = Math.degrees(trend);
+    trend = Math.round(trend);
+
+    return trend;
+}
+
+function fnis(plunge){
+
+    return Math.atan(plunge/Math.sqrt(1 - plunge * plunge));
+
 }
 
 function readInput(){
@@ -34,7 +122,7 @@ function readInput(){
             if(lineArr.length > 1)
             {
 
-                calculateTrendPlunge(lineArr[0], lineArr[1]);
+                calculateTrendPlunge(Number(lineArr[0]), Number(lineArr[1]), lineArr[2], Number(lineArr[3]), lineArr[4]);
 
             }
 
@@ -51,7 +139,7 @@ function readInput(){
             if(lineArr.length > 1)
             {
 
-                calculateTrendPlunge(lineArr[0], lineArr[1]);
+                calculateTrendPlunge(Number(lineArr[0]), Number(lineArr[1]), lineArr[2], Number(lineArr[3]), lineArr[4]);
 
             }
         }
@@ -60,31 +148,17 @@ function readInput(){
 }
 
 
-function calculateTrendPlunge(pitch, dip){
+function calculateTrendPlunge(strike, dip, dipdir, pitch, pitchdir){
 
-    pitch = Math.radians(pitch);
-    dip = Math.radians(dip);
+    var plunge = findPlunge(Number(pitch), Number(dip));
 
-    var trend = Math.atan(Math.tan(pitch) * Math.cos(dip));
-    var plunge = Math.atan(Math.tan(pitch) * Math.sin(dip));
+    var trend = findTrend(Number(strike), dipdir, pitchdir, Number(pitch), Number(dip));
 
-    if(trend < 0) {
-        trend = trend * -1;
+    if(trend === "Error"){
+        output += "Error" + "\n";
     }
-
-    if(plunge < 0){
-        plunge = plunge * -1;
-    }
-
-    trend = Math.degrees(trend);
-    plunge = Math.degrees(plunge);
-
-    trend = Math.round(trend);
-    plunge = Math.round(plunge);
-
-
-    output += trend + "\n";
-    outputTrend += plunge + "\n";
+    else output += Math.round(trend) + "\n";
+    outputTrend += Math.round(plunge) + "\n";
 
 }
 
